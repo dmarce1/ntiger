e8eb8afbfefe0c370d3a3a03645c358447c0aa10
Dominic Marcello <dmarce504@gmail.com>
2020-05-24 11:55:10 -0500

    Added missing function

--- src/tree/tree_compute.cpp
+++ src/tree/tree_compute.cpp
@@ -16,6 +16,28 @@ constexpr int NNGB = 32;
 #endif
 #endif
 
+void tree::advance_time(fixed_real t) {
+	static auto opts = options::get();
+	const auto use_grav = opts.gravity || opts.problem == "kepler";
+	if (leaf) {
+		PROFILE();
+		parts.resize(nparts0);
+		for (int i = 0; i < nparts0; i++) {
+			auto &p = parts[i];
+			if (p.t + p.dt == t || opts.global_time) {
+				p.t += p.dt;
+			}
+		}
+	} else {
+		std::array<hpx::future<void>, NCHILD> futs;
+		for (int ci = 0; ci < NCHILD; ci++) {
+			futs[ci] = hpx::async<advance_time_action>(children[ci], t);
+		}
+		hpx::wait_all(futs);
+	}
+}
+
+
 bool tree::adjust_timesteps(fixed_real t, int factor) {
 	static const auto opts = options::get();
 	bool rc = false;

55380ea0fe7cf4e84bd0a8765b8c5f064e457545
Dominic Marcello <dmarce504@gmail.com>
2020-05-31 01:49:19 -0500

    Fixed global time bug

--- src/tree/tree_gravity.cpp
+++ src/tree/tree_gravity.cpp
@@ -228,7 +228,7 @@ void tree::compute_gravity(std::vector<hpx::id_type> nids, std::vector<mass_attr
 			std::lock_guard < hpx::lcos::local::mutex > lock(*mtx);
 			int j = 0;
 			for (int i = 0; i < parts.size(); i++) {
-				if (parts[i].t + parts[i].dt == t + dt) {
+				if (opts.global || (parts[i].t + parts[i].dt == t + dt)) {
 					parts[i].g = parts[i].g + g[j].g;
 					parts[i].phi += g[j].phi;
 					j++;
